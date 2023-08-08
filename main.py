b7d5cc9ff0457e8362a0d716299b5fa2523ca45c
Ennio <Speedy565.55@outlook.com>
2023-08-08 15:05:37 +1200

    created UI, added 3 inputs

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import pygame
 import ui
 
-system = ui.UserInterface()
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+text_font = pygame.font.Font(None, 32)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Physics simulator')
+screen.fill(WHITE)
+pygame.display.update()
+
+system = ui.UserInterface(screen)
 
 run = True
 while run:
@@ -10,3 +29,34 @@ while run:
         #if an in=game event is activated and it is the QUIT event (closing the window), the program stops
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for text_input in system.text_inputs:
+                    if text_input['rect'].collidepoint(event.pos):
+                        text_input['active'] = True
+                    else:
+                        text_input['active'] = False
+
+            if event.type == pygame.KEYDOWN:
+                for text_input in system.text_inputs:
+                    if True in text_input.values():
+                        if event.key == pygame.K_BACKSPACE:
+                            text_input['text'] = text_input['text'][:-1]
+                        else:
+                            text_input['text'] += event.unicode
+
+    for text_input in system.text_inputs:
+        color = system.color_state(text_input['active'])
+        pygame.draw.rect(screen, color, text_input['rect'])
+
+        text_surface = text_font.render(text_input['text'], True, WHITE)
+        screen.blit(text_surface, (text_input['rect'].x+5, text_input['rect'].y+5))
+
+        screen.blit(text_input['label'], (text_input['rect'].x - 100, text_input['rect'].y + 5))
+    
+
+    pygame.display.flip()
+
+    clock.tick(2)
+    
+        
--- ui.py
+++ ui.py
@@ -9,17 +9,42 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 class UserInterface():
-    
-    def init(self) -> None:
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption('Physics simulator')
-        self.screen.fill(WHITE)
-        pygame.display.update()
 
-    def draw_input_box(self, x, y, surface) -> None:
-        user_text = ''
+    text_inputs = []
+
+    def __init__(self, surface) -> None:
+
+        label_font = pygame.font.Font(None, 32)
+
+        self.screen = surface
+
+        speed_text = label_font.render('SPEED', True, BLACK)
+        angle_text = label_font.render('ANGLE', True, BLACK)
+        height_text = label_font.render('HEIGHT', True, BLACK)
+
+        speed_input = {'text': '', 'active': False, 'rect': self.draw_input_box(150, 100), 'label': speed_text}
+        angle_input = {'text': '', 'active': False, 'rect': self.draw_input_box(150, 200), 'label': angle_text}
+        height_input = {'text': '', 'active': False, 'rect': self.draw_input_box(150, 300), 'label': height_text}
+
+        self.text_inputs.extend([speed_input, angle_input, height_input])
+
+    def draw_input_box(self, x, y) -> pygame.Rect:
         input_box = pygame.Rect(x, y, 140, 32)
+        return input_box
+    
+    def color_state(self, state):
+        if state == False:
+            color = pygame.Color('chartreuse4')
+        else:
+            color = pygame.Color('lightskyblue3')
+
+        return color
 
 
-        pass
+
+
+# https://www.geeksforgeeks.org/how-to-create-a-text-input-box-with-pygame/
+
+
+
+# MOUSE DOWN --> TURN INPUT ACTIVE --> ASSIGN DIFFERENT TEXT INPUTS FOR SEPERATE BOXES --> MOUSE DOWN OFF BOX --> NOT ACTIVE
